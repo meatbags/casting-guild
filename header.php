a021ea86ba7e630e39b2fdf446f93793a8de5f8d
Xavier Burrow <jxburrow@gmail.com>
2018-01-23 13:24:00 +1100

    theme setup

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<!-- by xavierburrow.com -->
+
+<html lang="en">
+<head>
+	<title><?php bloginfo('name'); ?></title>
+	<meta name="description" content="<?php bloginfo(); ?>">
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/lib/icons/favicon.png">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<?php wp_head(); ?>
+
+	<script>
+		/* <![CDATA[ */
+			var themePath = '<?php echo get_template_directory_uri(); ?>';
+			var ajaxUrl = '<?php echo admin_url('admin-ajax.php'); ?>';
+			var pageTitle = '<?php echo get_the_title(); ?>';
+			var isHome = '<?php echo is_home(); ?>';
+		/* ]]> */
+	</script>
+</head>
+
+<body class="<?php echo get_body_class(''); ?>">
+
+<div class="content">
+	<div class="wrapper">
+		<?php get_template_part('nav'); ?>
