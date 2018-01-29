c7cecc74f8f046e56c602011301eef575af0c6d2
Xavier Burrow <jxburrow@gmail.com>
2018-01-29 21:33:03 +1100

    minified

--- list-files.php
+++ list-files.php
@@ -4,13 +4,14 @@ if ($files): ?>
 
   <div class='file-list'>
   <?php foreach($files as $file):
+    $title = $file['file']['title'];
     $name = $file['file']['filename'];
     $url = $file['file']['url'];
     ?>
     <div class='file'>
       <a target='_blank' href='<?php echo $url; ?>'>
         <div class='file__inner'>
-          &rarr;&nbsp;&nbsp;<?php echo $name; ?>
+          &rarr;&nbsp;&nbsp;<?php echo $title; ?>
         </div>
       </a>
     </div>
